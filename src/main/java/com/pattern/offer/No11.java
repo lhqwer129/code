8544192bc8e63c6b978888837030de9e663db37f
lihui23 <lihui23@vipkid.com.cn>
2021-05-25 21:32:20 +0800

    no 11

---
+++ src/main/java/com/pattern/offer/No11.java
@@ -0,0 +1,36 @@
+package com.pattern.offer;
+
+/**
+ * <a href="https://leetcode-cn.com/problems/xuan-zhuan-shu-zu-de-zui-xiao-shu-zi-lcof/">
+ *     旋转数组的最小数字
+ *     </a> </br>
+ *  <tag>
+ *      * 二分查找
+ *  </tag>
+ *
+ * @author lihui
+ * @time 2021/5/25 9:10 下午
+ */
+public class No11 {
+
+    public int minArray(int[] numbers) {
+        int low = 0;
+        int high = numbers.length - 1;
+
+        while(low < high) {
+            int mid = low + (high - low) / 2;
+            if (numbers[mid] > numbers[high]) {
+                low = mid + 1;
+            } else if (numbers[mid] < numbers[high]) {
+                high = mid;
+            } else {
+                high -= 1;
+            }
+        }
+        return numbers[low];
+    }
+
+
+
+
+}
